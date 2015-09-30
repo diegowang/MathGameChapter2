fc8d40d64edf9e6c4efd53bc3e3ec40305c77812
Diego Wang <diegowang@yahoo.com>
2015-09-30 12:28:59 -0400

    Made answer buttons work:
    1. Moved some variables to the top so onClick can access them;
    2. Add onClick listeners to three answer buttons;
    3. Added onClick method to make answer buttons respond to click action;

--- app/src/main/java/com/example/hwang/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/example/hwang/mathgamechapter2/GameActivity.java
@@ -2,11 +2,18 @@ package com.example.hwang.mathgamechapter2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,7 @@ public class GameActivity extends AppCompatActivity {
         //Here we initialize all our variables
         int partA = 9;
         int partB = 9;
-        int correctAnswer = partA * partB;
+        correctAnswer = partA * partB;
         int wrongAnswer1 = correctAnswer - 1;
         int wrongAnswer2 = correctAnswer + 1;
 
@@ -27,11 +34,9 @@ or TextView class and base as well as link our new objects directly to the appro
 
         TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
 
-        Button buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
-
-        Button buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
-
-        Button buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+        buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+        buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+        buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
 
         //Now we use the setText method of the class on our objects
 //to show our variable values on the UI elements.
@@ -46,7 +51,50 @@ or TextView class and base as well as link our new objects directly to the appro
         buttonObjectChoice1.setText("" + correctAnswer);
         buttonObjectChoice2.setText("" + wrongAnswer1);
         buttonObjectChoice3.setText("" + wrongAnswer2);
+
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        //declare a new int to be used in all the cases
+        int answerGiven=0;
+        switch (view.getId()) {
+
+            case R.id.buttonChoice1:
+                //initialize a new int with the value contained in buttonObjectChoice1
+                //Remember we put it there ourselves previously
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+                //is it the right answer?
+                if(answerGiven==correctAnswer) {//yay it's the right answer
+                    Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
+                }else{//uh oh!
+                    Toast.makeText(getApplicationContext(),"Sorry that's wrong", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.buttonChoice2:
+                //same as previous case but using the next button
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+                if(answerGiven==correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Sorry that's wrong", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case R.id.buttonChoice3:
+                //same as previous case but using the next button
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+                if(answerGiven==correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Sorry that's wrong", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+        }
+    }
 }
